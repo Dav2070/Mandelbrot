86748eda8238a86d06da1f8e0a3bd674f984ba5c
Dav2070 <dav2070@web.de>
2017-05-01 20:58:42 +0200

    Add drawing method to MapPage

--- Mandelbrot/MapPage.xaml.cs
+++ Mandelbrot/MapPage.xaml.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -22,9 +27,43 @@ namespace Mandelbrot
     /// </summary>
     public sealed partial class MapPage : Page
     {
+        private ContainerVisual _root;
+
         public MapPage()
         {
             this.InitializeComponent();
+            CreateMap();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateMap()
+        {
+            _root = FileManager.GetVisual(ContentRoot);
+
+            // Get the current compositor
+            Compositor compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+
+            int factor = 8;
+            double pixelOverall = (Window.Current.Bounds.Width * Window.Current.Bounds.Height) / factor;
+            double pixelWidth = Window.Current.Bounds.Width/factor;
+            double pixelHeight = Window.Current.Bounds.Height/factor;
+
+            for (int y = 0; y < pixelHeight; y++)
+            {
+                for (int x = 0; x < pixelWidth; x++)
+                {
+                    SpriteVisual rect = compositor.CreateSpriteVisual();
+                    rect.Brush = compositor.CreateColorBrush(Colors.Black);
+                    rect.Size = new Vector2(factor, factor);
+                    rect.Offset = new Vector3(x*factor, y*factor, 0);
+                    _root.Children.InsertAtTop(rect);
+                }
+                Debug.WriteLine("Reihe " + y + " von " + pixelHeight);
+            }
         }
     }
 }
--- Mandelbrot/Models/FileManager.cs
+++ Mandelbrot/Models/FileManager.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Hosting;
 
 namespace Mandelbrot
 {
-    public class FileManager
+    public static class FileManager
     {
         public const double MAX_DIFFERENCE = 0.1;
         public const int MAX_PERIODIZITAET = 500;
         public const int ITERATIONEN = 10000;
+
+        public static ContainerVisual GetVisual(this UIElement element)
+        {
+            var hostVisual = ElementCompositionPreview.GetElementVisual(element);
+            var root = hostVisual.Compositor.CreateContainerVisual();
+            ElementCompositionPreview.SetElementChildVisual(element, root);
+            return root;
+        }
     }
 }
